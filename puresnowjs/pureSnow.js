3bcbdf4e8b1c3e16cd7ab9d7cf2d8dfdf3bb1251
hyperstown <experynt@gmail.com>
2025-04-22 22:29:46 +0200

    improvements for puresnow

--- puresnowjs/pureSnow.js
+++ puresnowjs/pureSnow.js
@@ -1,102 +1,139 @@
-let snowflakesCount = 200;
+let snowflakesCount = 200; // Snowflake count, can be overwritten by attrs
+let baseCSS = ``;
 
-// let baseCss = ``; // Put your custom base css here
 
-if (typeof total !== 'undefined'){
-    snowflakesCount = total;
+// set global attributes
+if (typeof SNOWFLAKES_COUNT !== 'undefined') {
+  snowflakesCount = SNOWFLAKES_COUNT;
+}
+if (typeof BASE_CSS !== 'undefined') {
+  baseCSS = BASE_CSS;
 }
 
-let bodyHeightPx = document.body.offsetHeight;
-let pageHeightVH = (100 * bodyHeightPx / window.innerHeight);
+let bodyHeightPx = null;
+let pageHeightVh = null;
+
+function setHeightVariables() {
+  bodyHeightPx = document.body.offsetHeight;
+  pageHeightVh = (100 * bodyHeightPx / window.innerHeight);
+}
+
+// get params set in snow div
+function getSnowAttributes() {
+  const snowWrapper = document.getElementById('snow');
+  snowflakesCount = Number(
+    snowWrapper?.dataset?.count || snowflakesCount
+  );
+}
 
 // This function allows you to turn on and off the snow
-function toggleSnow() {
-    let checkBox = document.getElementById("toggleSnow");
-    if (checkBox.checked == true) {
-        document.getElementById('snow').style.display = "block";
-    }
-    else {
-        document.getElementById('snow').style.display = "none";
-    }
+function showSnow(value) {
+  if (value) {
+    document.getElementById('snow').style.display = "block";
+  }
+  else {
+    document.getElementById('snow').style.display = "none";
+  }
 }
 
 // Creating snowflakes
-function spawnSnow(snowDensity = 200) {
-    snowDensity -= 1;
+function generateSnow(snowDensity = 200) {
+  snowDensity -= 1;
+  const snowWrapper = document.getElementById('snow');
+  snowWrapper.innerHTML = '';
+  for (let i = 0; i < snowDensity; i++) {
+    let board = document.createElement('div');
+    board.className = "snowflake";
+    snowWrapper.appendChild(board);
+  }
+}
 
-    for (let x = 0; x < snowDensity; x++) {
-        let board = document.createElement('div');
-        board.className = "snowflake";
+function getOrCreateCSSElement() {
+  let cssElement = document.getElementById("psjs-css");
+  if (cssElement) return cssElement;
 
-        document.getElementById('snow').appendChild(board);
-    }
+  cssElement = document.createElement('style');
+  cssElement.id = 'psjs-css';
+  document.head.appendChild(cssElement);
+  return cssElement;
 }
 
 // Append style for each snowflake to the head
-function addCss(rule) {
-    let css = document.createElement('style');
-    css.type = 'text/css';
-    css.appendChild(document.createTextNode(rule)); // Support for the rest
-    document.getElementsByTagName("head")[0].appendChild(css);
+function addCSS(rule) {
+  const cssElement = getOrCreateCSSElement();
+  cssElement.innerHTML = rule; // safe to use innerHTML
+  document.head.appendChild(cssElement);
 }
 
 // Math
-function randomInt(value = 100){
-    return Math.floor(Math.random() * value) + 1;
+function randomInt(value = 100) {
+  return Math.floor(Math.random() * value) + 1;
 }
 
-function randomRange(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function randomIntRange(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
 }
 
 // Create style for snowflake
-function spawnSnowCSS(snowDensity = 200){
-    let snowflakeName = "snowflake";
-    let rule = ``;
-    if (typeof baseCss !== 'undefined'){
-        rule = baseCss;
-    }
-    
-    for(let i = 1; i < snowDensity; i++){
-        let randomX = Math.random() * 100; // vw
-        let randomOffset = randomRange(-100000, 100000) * 0.0001; // vw;
-        let randomXEnd = randomX + randomOffset;
-        let randomXEndYoyo = randomX + (randomOffset / 2);
-        let randomYoyoTime = randomRange(30000, 80000) / 100000;
-        let randomYoyoY = randomYoyoTime * pageHeightVH; // vh
-        let randomScale = Math.random();
-        let fallDuration = randomRange(10, pageHeightVH / 10 * 3); // s
-        let fallDelay = randomInt(pageHeightVH / 10 * 3) * -1; // s
-        let opacity = Math.random();
+function generateSnowCSS(snowDensity = 200) {
+  let snowflakeName = "snowflake";
+  let rule = baseCSS;
 
-        rule += `
-        .${snowflakeName}:nth-child(${i}) {
-            opacity: ${opacity};
-            transform: translate(${randomX}vw, -10px) scale(${randomScale});
-            animation: fall-${i} ${fallDuration}s ${fallDelay}s linear infinite;
+  for (let i = 1; i < snowDensity; i++) {
+    let randomX = Math.random() * 100; // vw
+    let randomOffset = Math.random() * 10 // vw;
+    let randomXEnd = randomX + randomOffset;
+    let randomXEndYoyo = randomX + (randomOffset / 2);
+    let randomYoyoTime = getRandomArbitrary(0.3, 0.8);
+    let randomYoyoY = randomYoyoTime * pageHeightVh; // vh
+    let randomScale = Math.random();
+    let fallDuration = randomIntRange(10, pageHeightVh / 10 * 3); // s
+    let fallDelay = randomInt(pageHeightVh / 10 * 3) * -1; // s
+    let opacity = Math.random();
+
+    rule += `
+      .${snowflakeName}:nth-child(${i}) {
+        opacity: ${opacity};
+        transform: translate(${randomX}vw, -10px) scale(${randomScale});
+        animation: fall-${i} ${fallDuration}s ${fallDelay}s linear infinite;
+      }
+      @keyframes fall-${i} {
+        ${randomYoyoTime * 100}% {
+          transform: translate(${randomXEnd}vw, ${randomYoyoY}vh) scale(${randomScale});
         }
-
-        @keyframes fall-${i} {
-            ${randomYoyoTime*100}% {
-                transform: translate(${randomXEnd}vw, ${randomYoyoY}vh) scale(${randomScale});
-            }
-
-            to {
-                transform: translate(${randomXEndYoyo}vw, ${pageHeightVH}vh) scale(${randomScale});
-            }
-            
+        to {
+          transform: translate(${randomXEndYoyo}vw, ${pageHeightVh}vh) scale(${randomScale});
         }
-        `
-    }
-    addCss(rule);
+      }
+    `
+  }
+  addCSS(rule);
 }
 
 // Load the rules and execute after the DOM loads
-window.onload = function() {
-    spawnSnowCSS(snowflakesCount);
-    spawnSnow(snowflakesCount);
+function createSnow() {
+  setHeightVariables();
+  getSnowAttributes();
+  generateSnowCSS(snowflakesCount);
+  generateSnow(snowflakesCount);
 };
 
-// TODO add progress bar for slower clients
+
+window.addEventListener('resize', createSnow);
+
+// export createSnow function if using node or CommonJS environment
+if (typeof module !== 'undefined') {
+  module.exports = {
+    createSnow,
+    showSnow,
+  };
+}
+else {
+  window.onload = createSnow;
+}
